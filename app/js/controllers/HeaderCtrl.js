d0dfd316dab6bd888a83fd31cf081bf6dda65807
Anudeep Potlapally <anudeep.potlapally@gmail.com>
2014-09-10 00:44:14 -0500

    Added Google Anlytics Added missing images Changed header to a template

--- app/js/controllers/HeaderCtrl.js
+++ app/js/controllers/HeaderCtrl.js
@@ -1,7 +1,7 @@
 define(['angular'], function(angular) {
   'use strict';
 
-  var HeaderCtrl = function($scope, $http, $location) {
+  var HeaderCtrl = function($scope, $http, $location, $rootScope) {
     $http.get('api/menu')
       .success(function(data, status, headers, config) {
         $scope.links = data;
@@ -13,6 +13,20 @@ define(['angular'], function(angular) {
     $scope.isActive = function (viewLocation) {
       return viewLocation === $location.path();
     };
+    
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      ga('send', {
+        'hitType': 'event',          // Required.
+        'eventCategory': 'navigation',   // Required.
+        'eventAction': 'click',      // Required.
+        'eventLabel': next.$$route.originalPath
+      });
+      ga('send', 'pageview', {
+        'page': next.$$route.originalPath,
+        'title': next.$$route.controller
+      });
+    });
+
   };
 
   return HeaderCtrl;
